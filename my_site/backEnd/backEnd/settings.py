1e134ec087f07c6dc916e40b60d5d81440a51524
YebraL <Product4conker@gmail.com>
2023-01-31 18:18:16 -0600

    scafold is finished

--- my_site/backEnd/backEnd/settings.py
+++ my_site/backEnd/backEnd/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'backEnd_app',
+    
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,8 @@ WSGI_APPLICATION = 'backEnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mySite',
     }
 }
 
@@ -116,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- my_site/front_end/vite.config.js
+++ my_site/front_end/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // vite uses this as a prefix for href and src URLs
+  base: '/static/',
+  build: {
+      //where vite puts the bundled application
+      outDir:'../backEnd/static',
+      // delete old build when creating new
+      emptyOutDir: true,
+      sourcemap: true,
+  },
   plugins: [react()]
-})
+  })
