3a30cc7646e8da5c6bed9f54e6d1ecd17dd4fa9b
YebraL <Product4conker@gmail.com>
2023-03-29 13:13:39 -0500

    Not mine but learning from it

--- 3jsPractice/src/App.jsx
+++ 3jsPractice/src/App.jsx
@@ -1,12 +1,9 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
-
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-
   return (
-    
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
@@ -24,7 +21,7 @@ const App = () => {
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
