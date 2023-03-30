5edcc0be7fc4f5d23e48b9e912c88f7f3235d89b
YebraL <Product4conker@gmail.com>
2023-03-30 13:58:04 -0500

    My own attempt update 4

--- 3jsMine/src/components/Hero.jsx
+++ 3jsMine/src/components/Hero.jsx
@@ -17,11 +17,30 @@ const Hero = () => {
           >Hi, I'm <span className="text-[#915eff]">Luigi</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           Software Engineer and U.S Navy Mechanic <br className="sm:block hidden"/>with a Bachelor’s in Computer Science. Expertise in preventative
-maintenance, 
+maintenance
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+
     </section>
   )
 }
